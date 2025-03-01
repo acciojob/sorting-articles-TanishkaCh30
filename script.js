ae95b80601f52d7fee5e2696b392cb668e533d4f
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-03-01 18:36:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+let ul = document.getElementById("band");
+
+const bands = ['The Plot in You', 'The Devil Wears Prada',
+			   'Pierce the Veil', 'Norma Jean', 'The Bled', 
+			   'Say Anything', 'The Midway State', 'We Came as Romans',
+			   'Counterparts', 'Oh, Sleeper', 'A Skylit Drive',
+			   'Anywhere But Here', 'An Old Dog'];
+
+
+function sortBands(bands) {
+            return bands.sort((a, b) => {
+                const cleanA = a.replace(/^(a |an |the )/i, '').trim();
+                const cleanB = b.replace(/^(a |an |the )/i, '').trim();
+                return cleanA.localeCompare(cleanB);
+            });
+        }
+
+        const sortedBands = sortBands(bands);
+       
+
+        sortedBands.forEach(band => {
+            const li = document.createElement('li');
+            li.textContent = band;
+            ul.appendChild(li);
+        });
